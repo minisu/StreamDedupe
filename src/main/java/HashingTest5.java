09d1a4d1bdcb9e9953f6f039b8e4035dbe239199
Henrik Olsson <henrik.olsson@smartbear.com>
2014-01-10 15:35:11 +0100

    Now stores all data required to restore everything.

--- src/main/java/HashingTest5.java
+++ src/main/java/HashingTest5.java
@@ -1,4 +1,6 @@
 import com.google.common.collect.HashMultiset;
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Multiset;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
@@ -7,14 +9,17 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.Adler32;
 
 public class HashingTest5
 {
 	public static final int MIN_BLOCK_SIZE = 1024;
 	public static final int MAX_BLOCK_SIZE = 10240;
-	private Multiset<Long> occ = HashMultiset.create();
-	private BufferedOutputStream blockContent;
+	private Multiset<Integer> occ = HashMultiset.create();
+    private ListMultimap<Long, Integer> storedFiles = LinkedListMultimap.create(1000);
+    private BufferedOutputStream blockContent;
 	private Adler32 checksum = new Adler32();
 
 	@Test
@@ -23,18 +28,14 @@ public class HashingTest5
 		File file = new File( "output.txt" );
 		blockContent = new BufferedOutputStream( new FileOutputStream( file ));
 
-		byte[] f1 = FileUtils.readFileToByteArray( new File( "D:/soapui.org.htm" ) );
-		byte[] f2 = FileUtils.readFileToByteArray( new File( "D:/soapui.org2.htm" ) );
-		byte[] f3 = FileUtils.readFileToByteArray( new File( "D:/atlassian.com.html" ) );
-		byte[] f4 = FileUtils.readFileToByteArray( new File( "D:/atlassian.com2.html" ) );
-		byte[] f5 = FileUtils.readFileToByteArray( new File( "D:/reddit.com.html" ) );
-		byte[] f6 = FileUtils.readFileToByteArray( new File( "D:/reddit.com2.html" ) );
+		byte[] f1 = FileUtils.readFileToByteArray( new File( "C:\\Users\\Henrik\\Documents\\temp/d.html" ) );
+		byte[] f2 = FileUtils.readFileToByteArray( new File( "C:\\Users\\Henrik\\Documents\\temp/d2.html" ) );
 
 		long startTime = System.currentTimeMillis();
 		for( int i = 0; i < 1; i++ )
 		{
-			checksumForFile( f5 );
-			checksumForFile( f6 );
+			checksumForFile( f1 );
+			checksumForFile( f2 );
 		}
 		System.out.println( System.currentTimeMillis() - startTime );
 
@@ -43,6 +44,15 @@ public class HashingTest5
 		for( HashMultiset.Entry e : occ.entrySet() )
 			System.out.println( e );
 
+        for( Long timestamp : storedFiles.keySet() )
+        {
+			System.out.println( System.lineSeparator() + timestamp + ":" );
+            for( Integer hash : storedFiles.get(timestamp) )
+            {
+                System.out.println("  "+hash);
+            }
+        }
+
 		System.out.println("Output: " + file.getAbsolutePath());
 	}
 
@@ -50,14 +60,16 @@ public class HashingTest5
 	{
 		int p=0;
 		int length = data.length;
+        List<Integer> hashes = new ArrayList<>( data.length / MIN_BLOCK_SIZE );
 
 		while(p<length)
 		{
 			checksum.reset();
 			int i = 0;
-			int b = data[p];
+			int b;
 			while( p<length )
 			{
+                b = data[p];
 				if( b=='<' && i > MIN_BLOCK_SIZE && (data[p+1] == 'h' || data[p+1] == 'H') )
 						break;
 				if( i > MAX_BLOCK_SIZE )
@@ -65,19 +77,23 @@ public class HashingTest5
 					if( b=='<' && (data[p+1] == 'p' || data[p+1] == 'P') )
 						break;
 				}
-				b = data[p];
 				i++;
 				p++;
 			}
 			checksum.update( data, p - i, i );
 
-			long hash = checksum.getValue();
+			int hash = (int) checksum.getValue();
 			if( !occ.contains( hash ) )
 			{
+                blockContent.write(hash);
+                blockContent.write(i);
 				blockContent.write( data, p-i, i );
 			}
+			occ.add( hash );
 
-			occ.add( checksum.getValue() );
+            hashes.add(hash);
 		}
+
+        storedFiles.putAll(System.currentTimeMillis(), hashes );
 	}
 }
